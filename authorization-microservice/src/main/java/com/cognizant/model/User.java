01e880596658bb48c8bc4a93d5752205107ce5bb
Pratyush <pratyushshukla9333@gmail.com>
2021-08-19 02:30:41 +0530

    Added Testing To Pension Management System

---
+++ authorization-microservice/src/main/java/com/cognizant/model/User.java
@@ -0,0 +1,23 @@
+package com.cognizant.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+public class User {
+
+	@Id
+	private int id;
+	private String userName;
+	private String password;
+	
+
+}
